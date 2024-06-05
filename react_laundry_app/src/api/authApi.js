4dda86e429172a7f4c44bbae92e549b7e6285c91
danielpsrb <danielpsrb01@gmail.com>
2024-06-05 18:26:03 +0700

    init laravel sb admin 2 template

--- react_laundry_app/src/api/authApi.js
+++ react_laundry_app/src/api/authApi.js
@@ -22,3 +22,14 @@ export const loginUser = async (credentials) => {
         throw error;
     }
 };
+
+//logout user
+export const logoutUser = async () => {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/logout`);
+        return response.data;
+    } catch (error) {
+        console.error('Failed to logout user:', error);
+        throw error;
+    }
+};
