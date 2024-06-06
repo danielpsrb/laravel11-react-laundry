e0fd494909cc13f2a318c227d9a2dc14dbee01cf
danielpsrb <danielpsrb01@gmail.com>
2024-06-06 09:42:58 +0700

    init laravel stisla admin laundry

---
+++ laravel_react_laundry/resources/views/pages/auth/login.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.auth')
+
+@section('title', 'Login Admin Laundry')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet"
+        href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
+@endpush
+
+@section('main')
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4 class="text-center w-100">Welcome Admin, Please Login First</h4>
+        </div>
+
+        <div class="card-body">
+            <form method="POST"
+                action="{{ route('login') }}"
+                class="needs-validation"
+                novalidate="">
+                @csrf
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email"
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        tabindex="1"
+                        required
+                        autofocus>
+                    <div class="invalid-feedback">
+                        Please fill in your email
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="d-block">
+                        <label for="password"
+                            class="control-label">Password</label>
+                    </div>
+                    <input id="password"
+                        type="password"
+                        class="form-control"
+                        name="password"
+                        tabindex="2"
+                        required>
+                    <div class="invalid-feedback">
+                        please fill in your password
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox"
+                            class="custom-control-input"
+                            tabindex="3"
+                            id="show-password"
+                            onclick="togglePassword()">
+                        <label class="custom-control-label"
+                            for="show-password">Show Password</label>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit"
+                        class="btn btn-primary btn-lg btn-block"
+                        tabindex="4">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+    <script>
+        function togglePassword() {
+            var passwordInput = document.getElementById('password');
+            var showPasswordCheckbox = document.getElementById('show-password');
+            if (showPasswordCheckbox.checked) {
+                passwordInput.type = 'text';
+            } else {
+                passwordInput.type = 'password';
+            }
+        }
+    </script>
+@endpush
