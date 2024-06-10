c22d7945c3d3580921623eb6ed2383cf577f9a82
danielpsrb <danielpsrb01@gmail.com>
2024-06-10 21:46:54 +0700

    add components footer to home pages

--- react_laundry_app/src/pages/Home/Home.jsx
+++ react_laundry_app/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../../pages/Home/home.css";
 import Navbar from "../../components/Navbar";
 import HeroSection from "../../components/HeroSection";
+import Footer from "../../components/Footer";
 
 function Home() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <Footer />
     </div>
   );
 }
