79aa04ba7628eda90a17ba3f7d0150a4e1a72bc9
danielpsrb <danielpsrb01@gmail.com>
2024-06-10 13:32:56 +0700

    update order dan user controller

--- laravel_react_laundry/app/Http/Controllers/OrderController.php
+++ laravel_react_laundry/app/Http/Controllers/OrderController.php
@@ -17,6 +17,12 @@ class OrderController extends Controller
 
         $order = Order::create($data);
 
+        // Perbarui data alamat dan nomor telepon di tabel users
+        $user = Auth::user();
+        $user->address = $data['alamat'];
+        $user->phone_number = $data['nomor_telepon'];
+        $user->save();
+
         return response()->json([
             'message' => 'Order created successfully',
             'order' => $order
@@ -24,9 +30,9 @@ class OrderController extends Controller
     }
 
       // mengambil daftar pesanan
-      public function getOrders(Request $request)
-      {
-          $orders = Order::where('user_id', $request->user()->id)->get();
-          return response()->json($orders);
-      }
+    public function getOrders(Request $request)
+    {
+        $orders = Order::where('user_id', $request->user()->id)->get();
+        return response()->json($orders);
+    }
 }
--- laravel_react_laundry/app/Http/Controllers/UserController.php
+++ laravel_react_laundry/app/Http/Controllers/UserController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::where('name', 'like', "%".request('name')."%")
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
 }
