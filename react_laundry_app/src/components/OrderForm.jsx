198eddefea10f1f22e48e30597e6b7543c13207f
danielpsrb <danielpsrb01@gmail.com>
2024-06-11 13:16:03 +0700

    update order form dan orders list

--- react_laundry_app/src/components/OrderForm.jsx
+++ react_laundry_app/src/components/OrderForm.jsx
@@ -1,27 +1,32 @@
-// src/components/OrderForm.js
 import React from "react";
 
-const OrderForm = ({orderData, handleChange, handleSubmit}) => {
+const OrderForm = ({ orderData, handleChange, handleSubmit }) => {
   return (
     <form className='d-flex flex-column gap-3 col-6 p-3' onSubmit={handleSubmit}>
       <div>
-        <label>Name:</label>
-        <input type='text' name='name' value={orderData.name} onChange={handleChange} />
+        <label>Nama:</label>
+        <input type='text' className="form-control" name='name' value={orderData.name} onChange={handleChange} />
       </div>
       <div>
         <label>Alamat:</label>
-        <input type='text' name='alamat' value={orderData.alamat} onChange={handleChange} />
+        <input type='text' className="form-control" name='alamat' value={orderData.alamat} onChange={handleChange} />
+      </div>
+      <div>
+        <label>Nomor Telepon:</label>
+        <input type='tel' className="form-control" name='nomor_telepon' value={orderData.nomor_telepon} onChange={handleChange} />
       </div>
       <div>
         <label>Jenis Laundry:</label>
         <select name='jenis_laundry' value={orderData.jenis_laundry} onChange={handleChange}>
-          <option value='cuci_lipat'>Cuci Lipat</option>
-          <option value='cuci_setrika'>Cuci Setrika</option>
+          <option value='' disabled>Pilih Jenis Laundry</option>
+          <option value='Cuci Lipat'>Cuci Lipat</option>
+          <option value='Cuci Setrika'>Cuci Setrika</option>
         </select>
       </div>
       <div>
         <label>Estimasi Hari:</label>
         <select name='estimasi_hari' value={orderData.estimasi_hari} onChange={handleChange}>
+          <option value='' disabled>Pilih Estimasi Hari</option>
           <option value='1'>1 Hari (Express)</option>
           <option value='3'>3 Hari (Reguler)</option>
         </select>
--- react_laundry_app/src/components/OrdersList.jsx
+++ react_laundry_app/src/components/OrdersList.jsx
@@ -7,9 +7,11 @@ const OrderList = ({orders}) => {
         <tr>
           <th scope='col'>Name</th>
           <th scope='col'>Alamat</th>
+          <th scope='col'>No Telp</th>
           <th scope='col'>Jenis Laundry</th>
           <th scope='col'>Estimasi Hari</th>
           <th scope='col'>Status</th>
+          {/* <th scope='col'>Action</th> */}
         </tr>
       </thead>
       <tbody>
@@ -17,9 +19,13 @@ const OrderList = ({orders}) => {
           <tr key={order.id}>
             <td>{order.name}</td>
             <td>{order.alamat}</td>
+            <td>{order.nomor_telepon}</td>
             <td>{order.jenis_laundry}</td>
             <td>{order.estimasi_hari}</td>
             <td>{order.status}</td>
+            {/* <td>
+              <button className='btn btn-primary'>Edit</button>
+            </td> */}
           </tr>
         ))}
       </tbody>
