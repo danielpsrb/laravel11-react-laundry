768b3f8be2b6caf68c52149ca0878eedc6fa9e83
danielpsrb <danielpsrb01@gmail.com>
2024-06-11 13:10:15 +0700

    update pages show users

--- laravel_react_laundry/resources/views/pages/users/index.blade.php
+++ laravel_react_laundry/resources/views/pages/users/index.blade.php
@@ -13,7 +13,7 @@
             <div class="section-header">
                 <h1>Users</h1>
                 <div class="section-header-button">
-                    <a href="{{ route('users.create') }}" class="btn btn-primary">Add New</a>
+                    <a href="#" class="btn btn-primary">Add New</a>
                 </div>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
@@ -22,11 +22,11 @@
                 </div>
             </div>
             <div class="section-body">
-                <div class="row">
-                    {{-- <div class="col-12">
+                {{-- <div class="row">
+                    <div class="col-12">
                         @include('layouts.alert')
-                    </div> --}}
-                </div>
+                    </div>
+                </div> --}}
                 <h2 class="section-title">Users</h2>
                 <p class="section-lead">
                     You can manage all Users, such as editing, deleting and more.
@@ -42,7 +42,7 @@
                             <div class="card-body">
 
                                 <div class="float-right">
-                                    <form method="GET" action="{{ route('users.index') }}">
+                                    <form method="GET" action="{{ route('users.data') }}">
                                         <div class="input-group">
                                             <input type="text" class="form-control" placeholder="Search" name="name">
                                             <div class="input-group-append">
