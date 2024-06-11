8530f0887037b06f22beaf2791d2325a7c0355b6
danielpsrb <danielpsrb01@gmail.com>
2024-06-11 13:06:23 +0700

    update routes web

--- laravel_react_laundry/routes/web.php
+++ laravel_react_laundry/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\OrderAdminController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,7 +10,25 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('home', function () {
         return view('pages.dashboard', ['type_menu' => 'dashboard']);
-    })->name('dashboard');
+    })->name('home');
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.data');
+
+    //route orders
+    Route::get('/orders', [OrderAdminController::class, 'index'])->name('orders.data');
+
+    //route order-detail
+    Route::get('/orders/{id}', [OrderAdminController::class, 'show'])->name('orders.detail');
+
+    //route edit orders
+    Route::get('/orders/{id}/edit', [OrderAdminController::class, 'edit'])->name('orders.edit');
+
+    //route update orders
+    Route::patch('/orders/{id}', [OrderAdminController::class, 'update'])->name('orders.update');
+
+    //route delete orders
+    Route::delete('/orders/{id}', [OrderAdminController::class, 'destroy'])->name('orders.delete');
+
 });
