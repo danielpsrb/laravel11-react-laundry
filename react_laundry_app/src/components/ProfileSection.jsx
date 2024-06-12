8090313752dde9892efda60c9f31e28d38707619
danielpsrb <danielpsrb01@gmail.com>
2024-06-12 20:47:32 +0700

    update components profile section

--- react_laundry_app/src/components/ProfileSection.jsx
+++ react_laundry_app/src/components/ProfileSection.jsx
@@ -10,11 +10,17 @@ function ProfileSection({ users }) {
                 {users && users.data ? (
                     <div className='text-center mt-5'>
                         <h2 className="fs-1 fw-semibold">Hello, {users.data.name}</h2>
+                        <div className='card'>
+                            <img src={users.data.images_profile_url || '/images/icon-person.png' } className='card-img-top' alt='profiles_image' />
+                            <h2 className="card-title">Profile</h2>
+                            <p>Username: {users.data.name}</p>
+                        </div>
                     </div>
                 ) : (
                     <p>Loading...</p>
                 )}
             </div>
+            
         </div>
     );
 }
