08e195ef5002449c20da1bea9f30b748c2d998a7
danielpsrb <danielpsrb01@gmail.com>
2024-06-12 12:20:00 +0700

    update user controller

--- laravel_react_laundry/app/Http/Controllers/UserController.php
+++ laravel_react_laundry/app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,5 +14,43 @@ class UserController extends Controller
             ->orderBy('id', 'desc')
             ->paginate(10);
         return view('pages.users.index', compact('users'));
+    }
+
+    //edit users
+    public function edit(User $user)
+    {
+        return view('pages.users.edit', compact('user'));
+    }
+
+    //update data users
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'address' => $request->address,
+        ]);
+
+        if ($request->password) {
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }
+
+        if ($request->hasFile('photo')) {
+            $image = $request->file('photo');
+            $imageName = $image->hashName();
+            $image->storeAs('public/profiles', $imageName);
+            $user->images_profile = $imageName;
+        }
+        $user->save();
+
+        return redirect()->route('users.data')->with('success', 'Data user berhasil diupdate');
     }
 }
