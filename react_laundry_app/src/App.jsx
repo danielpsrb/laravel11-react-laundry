66649d3f7f0010df09312e97cbc061d76347b4ce
danielpsrb <danielpsrb01@gmail.com>
2024-06-18 20:26:52 +0700

    add component protected routes to some pages

--- react_laundry_app/src/App.jsx
+++ react_laundry_app/src/App.jsx
@@ -6,21 +6,23 @@ import "./App.css";
 import Orders from "./pages/Orders/Orders";
 import ProfileUsers from "./pages/Profile/ProfileUsers";
 import NewOrder from "./pages/NewOrder/NewOrder"; // Import NewOrder component
+import { AuthProvider } from "./context/auth";
+import { ProtectedRoutes } from "./components/ProtectedRoutes";
 
 function App() {
   return (
-    <div>
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<SignIn />} />
-          <Route path='/register' element={<SignUp />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/orders' element={<Orders />} />
-          <Route path='/orders/new' element={<NewOrder />} />
-          <Route path='/profile' element={<ProfileUsers />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/home" element={<ProtectedRoutes><Home /></ProtectedRoutes>} />
+          <Route path="/orders" element={<ProtectedRoutes><Orders /></ProtectedRoutes>} />
+          <Route path="/profile" element={<ProtectedRoutes><ProfileUsers /></ProtectedRoutes>} />
+          <Route path="/orders/new" element={<ProtectedRoutes><NewOrder /></ProtectedRoutes>} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </AuthProvider>
   );
 }
 
